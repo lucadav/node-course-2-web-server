587940c197aeb62eef7d48ce9b804c58fd3ee602
Luca D'avino <luca.davino1996@gmail.com>
2018-02-17 12:23:40 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toDateString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log',log + '\n', (err) => {
+        if (err){
+            console.log('unable to write');
+        }
+    });
+
+    res.render('working.hbs',{
+        pageTitle: 'Home page',
+        welcomeMessage: 'welcome to my website'
+        
+    });
+    next();
+})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('scream', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs',{
+      pageTitle: 'Home page',
+      welcomeMessage: 'welcome to my website'
+      
+  })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+        
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+      erroMessage: 'unable to connect'
+    });
+});
+
+app.listen(3000);
